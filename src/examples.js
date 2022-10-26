77ba80dfa03ffef09ff3d648601b557576ba63d7
enchil <enchiliang@gmail.com>
2022-10-26 12:01:35 +0800

    把元件們統整成陣列

---
+++ src/examples.js
@@ -0,0 +1,21 @@
+import Counter from './ex-1025-2/Counter'
+import JsxValue from './ex-1026-1/JsxValue'
+import StudentList from './ex-1026-2/StudentList'
+
+export const examples = [
+  {
+    name: '計數器',
+    path: './ex-1025-2/Counter',
+    component: Counter,
+  },
+  {
+    name: '1026-1 JsxValue 表達式',
+    path: './ex-1026-1/JsxValue',
+    component: JsxValue,
+  },
+  {
+    name: '1026-2 學生列表清單',
+    path: './ex-1026-2/StudentList',
+    component: StudentList,
+  },
+]
