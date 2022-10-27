486efe5005d122a8aaa68fc5fed1d6030b02a132
enchil <enchiliang@gmail.com>
2022-10-27 09:44:32 +0800

    加入元件

--- src/examples.js
+++ src/examples.js
@@ -1,6 +1,8 @@
 import Counter from './ex-1025-2/Counter'
 import JsxValue from './ex-1026-1/JsxValue'
 import StudentList from './ex-1026-2/StudentList'
+import Menu from './pratice/Q1/Menu'
+import Demo from './ex-1026-3/Demo'
 
 export const examples = [
   {
@@ -17,5 +19,15 @@ export const examples = [
     name: '1026-2 學生列表清單',
     path: './ex-1026-2/StudentList',
     component: StudentList,
+  },
+  {
+    name: '練習Ｑ1',
+    path: './pratice/Q1/Menu',
+    component: Menu,
+  },
+  {
+    name: '1026-3 Demo',
+    path: './ex-1026-3/Demo',
+    component: Demo,
   },
 ]
