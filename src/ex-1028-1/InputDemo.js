5cb3f98b7b780369cd9d4785ae66845199298681
enchil <enchiliang@gmail.com>
2022-10-31 15:04:52 +0800

    可控表單元件

---
+++ src/ex-1028-1/InputDemo.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import InputById from './InputById'
+import InputByRefs from './InputByRefs'
+
+function InputDemo(params) {
+  const [inputText, setInputText] = useState('')
+
+  return (
+    <>
+      <h1>可控表單元件</h1>
+      <input
+        type="text"
+        value={inputText}
+        onChange={(e) => {
+          setInputText(e.target.value)
+        }}
+      />
+      <button
+        onClick={() => {
+          setInputText('test@gmail.com')
+        }}
+      >
+        載入資料
+      </button>
+      <hr />
+
+      <h1>不可控表單元件(id)</h1>
+      <InputById />
+      <InputById />
+
+      <hr />
+
+      <h1>不可控表單元件(ref)</h1>
+      <InputByRefs />
+      <InputByRefs />
+    </>
+  )
+}
+
+export default InputDemo
