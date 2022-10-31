a6722bd5decda7544d4f650a50fff214345a1560
enchil <enchiliang@gmail.com>
2022-10-31 15:05:38 +0800

    useEffect

--- src/ex-1027-3/ChildB.js
+++ src/ex-1027-3/ChildB.js
@@ -1,14 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function ChildB(props) {
   const [childBData, setChildBData] = useState('child B 的Data')
+
+  useEffect(() => {
+    props.setDataFromChildB(childBData)
+  }, [childBData])
+
   return (
     <>
       <h1>ChildB</h1>
       <button
-        onClick={() => {
-          props.setDataFromChildB(childBData)
-        }}
+        onClick={
+          (() => {
+            props.setDataFromChildB(childBData)
+          },
+          [childBData])
+        }
       >
         送資料回Parent
       </button>
